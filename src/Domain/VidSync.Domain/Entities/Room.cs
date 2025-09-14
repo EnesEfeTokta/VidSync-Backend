f4896edeea44a59347db388bba2ab683aa732666
Enes Efe Tokta <enesefetokta009@gmail.com>
2025-09-14 09:02:38 +0300

    Add Room entity and migration
    Introduced the Room entity to the domain model and updated AppDbContext to include Rooms. Added EF Core migration to create the Rooms table with relevant properties and constraints.

---
+++ src/Domain/VidSync.Domain/Entities/Room.cs
@@ -0,0 +1,10 @@
+namespace VidSync.Domain.Entities;
+
+public class Room
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+    public DateTime? ExpiresAt { get; set; }
+    public bool IsActive { get; set; }
+}
--- src/Infrastructure/VidSync.Persistence/AppDbContext.cs
+++ src/Infrastructure/VidSync.Persistence/AppDbContext.cs
@@ -13,10 +13,12 @@ public class AppDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
     }
 
     public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Room> Rooms { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
         modelBuilder.Entity<User>(entity =>
         {
             entity.ToTable("Users");
@@ -26,6 +28,16 @@ public class AppDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
             entity.Property(e => e.LastName).IsRequired(true).HasMaxLength(50);
             entity.Property(e => e.ProfilePictureUrl).IsRequired(false).HasMaxLength(256).HasDefaultValue("https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?semt=ais_hybrid&w=740");
             entity.Property(e => e.CreatedAt).IsRequired(true).HasColumnType("timestamp with time zone").HasDefaultValueSql("NOW()");
+        });
+
+        modelBuilder.Entity<Room>(entity =>
+        {
+            entity.ToTable("Rooms");
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).IsRequired(true).HasMaxLength(100);
+            entity.Property(e => e.CreatedAt).IsRequired(true).HasColumnType("timestamp with time zone").HasDefaultValueSql("NOW()");
+            entity.Property(e => e.ExpiresAt).IsRequired(false).HasColumnType("timestamp with time zone");
+            entity.Property(e => e.IsActive).IsRequired(true).HasDefaultValue(true);
         });
     }
 }
